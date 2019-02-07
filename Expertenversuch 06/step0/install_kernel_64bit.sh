1656da374d3a1963ed8862b7b5c29c4dc9d5d2d8
BB20101997 <bb-github@t-online.de>
2019-02-07 02:03:19 +0100

    more dokumentation changes and added kernel downgrade scripst from lms

---
+++ Expertenversuch 06/step0/install_kernel_64bit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+mkdir tmp
+cd tmp
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-headers-4.14.0-041400_4.14.0-041400.201711122031_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-headers-4.14.0-041400-generic_4.14.0-041400.201711122031_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-image-4.14.0-041400-generic_4.14.0-041400.201711122031_amd64.deb
+
+dpkg -i *.deb
+
+sed -i '/GRUB_HIDDEN_TIMEOUT=0/d' /etc/default/grub
+
+update-grub
+
+cd ..
+rm -rf tmp
+
+echo "done. Please reboot"
