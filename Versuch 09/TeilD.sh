7a36faec25a2ccb3a0428c8c39484954cad01d16
BB20101997 <bennet-blessmann@t-online.de>
2019-03-14 14:25:35 +0100

    V9 Teil D

--- Versuch 09/TeilD.sh
+++ Versuch 09/TeilD.sh
@@ -1,6 +1,7 @@
 token=qh3ivl
+msg="Moin Moin"
 curl -X POST \
-	-F "token=${token}" \
-	-F "msg=test" \
+	-k \
+	-d "token=${token}&msg=${msg}" \
 	--proxy 10.42.0.1:8080 \
 	https://itsec.sbx320.net/chat
